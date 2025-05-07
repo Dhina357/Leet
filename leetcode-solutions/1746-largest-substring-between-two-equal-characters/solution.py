7533d9e828d03140e089271c4f68ed99ecca4d66
Dhinagaran M <dhinagaran357@gmail.com>
2025-05-07 12:34:07 +0000

    Sync LeetCode submission - Largest Substring Between Two Equal Characters - Runtime - 0 ms (100.00%), Memory - 12.5 MB (20.66%)

---
+++ leetcode-solutions/1746-largest-substring-between-two-equal-characters/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maxLengthBetweenEqualCharacters(self, s):
+        for i in range(len(s) - 1, 0, -1):
+            for j in range(len(s) - i):
+                if s[j] == s[j + i]:
+                    return i - 1
+        return -1
