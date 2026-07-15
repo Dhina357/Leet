006579c7c74e9e2a97782781f57c3dd729752ec6
Dhinagaran M <dhinagaran357@gmail.com>
2026-07-15 15:58:09 +0000

    Sync LeetCode submission - Rotate String - Runtime - 0 ms (100.00%), Memory - 8.6 MB (52.74%)

---
+++ leetcode-solutions/0812-rotate-string/solution.c
@@ -0,0 +1,9 @@
+bool rotateString(char* s, char* goal) {
+    if(strlen(s)!=strlen(goal))return false;
+    char *doubled=(char*)malloc((2*strlen(s)+1)*sizeof(char));
+    strcpy(doubled,s);
+    strcat(doubled,s);
+    bool result=(strstr(doubled,goal)!=0);
+     free(doubled);
+     return result;
+}
