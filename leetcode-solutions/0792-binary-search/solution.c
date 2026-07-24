befb7e9f6af22ae449edd4b34f9a126c9fe0872d
Dhinagaran M <dhinagaran357@gmail.com>
2026-07-24 03:42:41 +0000

    Sync LeetCode submission - Binary Search - Runtime - 0 ms (100.00%), Memory - 10.1 MB (46.87%)

---
+++ leetcode-solutions/0792-binary-search/solution.c
@@ -0,0 +1,18 @@
+int binary(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
+    int mid=-1;
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+          return mid;
+        else if (nums[mid] < target) {
+           low=mid+1;
+        } else {
+           high=mid-1;
+        }
+    }
+    return -1;
+}
+int search(int* nums, int numsSize, int target) {
+    return binary(nums, numsSize, target);
+}
