518333bbc35d630815825de640358c3faa088458
Dhinagaran M <dhinagaran357@gmail.com>
2026-07-24 04:19:24 +0000

    Sync LeetCode submission - Search Insert Position - Runtime - 0 ms (100.00%), Memory - 8.7 MB (99.69%)

--- leetcode-solutions/0035-search-insert-position/solution.c
+++ leetcode-solutions/0035-search-insert-position/solution.c
@@ -1,8 +1,17 @@
 int searchInsert(int* nums, int numsSize, int target) {
-    for(int i=0;i<numsSize;i++){
-        if(target<=nums[i]){
-            return i;
+   
+    int low = 0, high = numsSize - 1;
+    int mid=-1;
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+          return mid;
+        else if (nums[mid] < target) {
+           low=mid+1;
+        } else {
+           high=mid-1;
         }
-        }
-    return numsSize;
+    }
+    return low;
 }
+
