8be17da471b58670413053109f3d9747c9504615
Dhinagaran M <dhinagaran357@gmail.com>
2026-07-28 05:34:35 +0000

    Sync LeetCode submission - Reverse Odd Levels of Binary Tree - Runtime - 3 ms (47.76%), Memory - 77 MB (92.04%)

---
+++ leetcode-solutions/2493-reverse-odd-levels-of-binary-tree/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+void rec(TreeNode* left,TreeNode* right,int level){
+    if(left==nullptr&&right==nullptr)return;
+    if(level%2==1)swap(left->val,right->val);
+    
+    rec(left->left,right->right,level+1);
+    rec(left->right,right->left,level+1);
+   
+}
+
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if(root==nullptr)return root;
+        rec(root->left,root->right,1);
+        return root;
+    }
+};
