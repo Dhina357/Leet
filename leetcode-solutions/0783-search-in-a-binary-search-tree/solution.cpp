387e4e5491a951f540e8a5de347b84022f6adac6
Dhinagaran M <dhinagaran357@gmail.com>
2026-08-12 07:57:27 +0000

    Sync LeetCode submission - Search in a Binary Search Tree - Runtime - 0 ms (100.00%), Memory - 35.6 MB (31.79%)

---
+++ leetcode-solutions/0783-search-in-a-binary-search-tree/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* rec(TreeNode* node,int val){
+        if(node==NULL)return NULL;
+        if(node->val==val)return node;
+         return  (val<node->val)?rec(node->left,val): rec(node->right,val);
+      
+    }
+    TreeNode* searchBST(TreeNode* root, int val) {
+        return rec(root,val);
+    }
+};
