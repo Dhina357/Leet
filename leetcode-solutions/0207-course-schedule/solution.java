ea66e4c1ea3b7c522d342135bfa1b47f14f7eba7
Dhinagaran M <dhinagaran357@gmail.com>
2026-08-19 05:44:35 +0000

    Sync LeetCode submission - Course Schedule - Runtime - 7 ms (56.54%), Memory - 47.1 MB (64.44%)

---
+++ leetcode-solutions/0207-course-schedule/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        int[] indegree=new int[numCourses];
+        for(int[] deg:prerequisites){
+            int a=deg[0];
+            int b=deg[1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        int[] order =new int[numCourses];
+        Queue<Integer>q=new LinkedList<>();
+   
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)q.offer(i);
+        }
+        int idx=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            order[idx++]=node;
+        for(int val:adj.get(node)){
+            indegree[val]--;
+            if(indegree[val]==0)
+            q.offer(val);
+        }
+        } 
+        if(idx==numCourses)
+        return true;
+        return false;
+    }
+}
