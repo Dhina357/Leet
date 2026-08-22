45213acb49e3316ac9b67f0ffa4ffa454ffee4fa
Dhinagaran M <dhinagaran357@gmail.com>
2026-08-22 10:29:29 +0000

    Sync LeetCode submission - Minimum Number of Arrows to Burst Balloons - Runtime - 52 ms (68.86%), Memory - 95.8 MB (34.49%)

---
+++ leetcode-solutions/0452-minimum-number-of-arrows-to-burst-balloons/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+ Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+         int arrow=points[0][1];
+        int count=1;
+        for(int i=0;i<points.length;i++){
+            if(arrow<points[i][0]){
+                count++;
+                arrow=points[i][1];
+            }
+        }
+        return count;
+    }
+}
