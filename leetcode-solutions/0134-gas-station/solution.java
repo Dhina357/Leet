a5a98cf954bdb587f68b613709b19d27270ccf37
Dhinagaran M <dhinagaran357@gmail.com>
2026-08-25 04:28:10 +0000

    Sync LeetCode submission - Gas Station - Runtime - 2 ms (99.20%), Memory - 118.9 MB (97.72%)

---
+++ leetcode-solutions/0134-gas-station/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas=0;
+        int totalCost=0;
+        int currTank=0;
+        int start=0;
+        
+        for(int i=0;i<gas.length;i++){
+            totalGas+=gas[i];
+            totalCost+=cost[i];
+            currTank+=(gas[i]-cost[i]);
+            if(currTank<0){
+                currTank=0;
+                start=i+1;
+            }
+
+        }
+        if(totalGas<totalCost)return -1;
+        return start;
+    }
+}
+
