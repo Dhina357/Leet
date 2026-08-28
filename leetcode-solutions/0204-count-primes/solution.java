3059ba211eae259ba7b506ed2df9dcfe1d2ac36a
Dhinagaran M <dhinagaran357@gmail.com>
2026-08-28 04:51:17 +0000

    Sync LeetCode submission - Count Primes - Runtime - 619 ms (24.09%), Memory - 79.8 MB (17.11%)

---
+++ leetcode-solutions/0204-count-primes/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+   
+    public int countPrimes(int n) {
+        boolean[] prime=new boolean[n];
+       
+        
+        int count=0;
+        if(n<2)return count;
+        for(int i=2;i*i<n;i++){
+            if(!prime[i]){
+                
+            for(int j=i*i;j<n;j+=i){
+                prime[j]=true;
+            }}
+        }
+        for(int i=2;i<n;i++){
+            if(!prime[i])count++;
+        }
+        return count;
+    }
+}
