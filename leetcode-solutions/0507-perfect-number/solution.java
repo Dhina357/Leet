b0c345b7eecc7556610d2cdea4b0a96b8d146777
Dhinagaran M <dhinagaran357@gmail.com>
2026-08-29 05:53:38 +0000

    Sync LeetCode submission - Perfect Number - Runtime - 2 ms (83.74%), Memory - 41.8 MB (94.54%)

---
+++ leetcode-solutions/0507-perfect-number/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+       
+        int sum=0;
+        for(int i=1;i*i<=num;i++){
+            if(num%i==0&&i!=num){
+                sum+=i;
+                if(num/i!=i&&num/i!=num)sum+=num/i;
+            }
+        }
+        return sum==num;
+    }
+}
