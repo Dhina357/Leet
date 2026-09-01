02070c84fa3c84a31a232e034d731f186adff1b2
Dhinagaran M <dhinagaran357@gmail.com>
2026-09-01 08:18:39 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters - Runtime - 299 ms (17.84%), Memory - 81.4 MB (15.55%)

--- leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> os;
+        unordered_map<char,int> os;
         int ma = 0, l = 0;
         for (int r = 0; r < s.length(); r++) {
 
-            while(os.count(s[r])) {
+            while(os.count(s[r])!=0) {
                 os.erase(s[l]);
                 l++;
 
             }
-            os.insert(s[r]);
+            os[s[r]]++;
             ma=max(ma,r-l+1);
         }
         return ma;
